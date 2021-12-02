16da4b296000c9a31f285d78e51cfc2c25a4f440
Jacob Hamblin <jacob@hamblin.cc>
2021-12-01 20:40:01 -0700

    part two

--- puzzles/day01.py
+++ puzzles/day01.py
@@ -4,3 +4,19 @@ def numIncreases(depths):
         if depths[i] > depths[i - 1]:
             count += 1
     return count
+
+
+def increasesSlidingWindow(depths):
+    count = 0
+    cur_window = 0
+    i = 0
+    for j in range(len(depths)):
+        new_window = cur_window
+        new_window += depths[j]
+        if j - i > 2:
+            new_window -= depths[i]
+            i += 1
+            if new_window > cur_window:
+                count += 1
+        cur_window = new_window
+    return count
--- tests/day01.py
+++ tests/day01.py
@@ -6,24 +6,24 @@ from tests import helpers
 expect = helpers.expect_equal
 
 
-def test_sample_data():
-    depths = [
-        199,
-        200,
-        208,
-        210,
-        200,
-        207,
-        240,
-        269,
-        260,
-        263,
-    ]
-    expect(day01.numIncreases(depths), 7)
+sample_depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+my_file = open("input/day01.txt", "r")
+content = my_file.readlines()
+depths = [int(item.strip()) for item in content]
 
 
-def test_real_input():
-    my_file = open("input/day01.txt", "r")
-    content = my_file.readlines()
-    depths = [int(item.strip()) for item in content]
+def test_part1_sample_input():
+    expect(day01.numIncreases(sample_depths), 7)
+
+
+def test_part1_provided_input():
     expect(day01.numIncreases(depths), 1766)
+
+
+def test_part2_sample_input():
+    expect(day01.increasesSlidingWindow(sample_depths), 5)
+
+
+def test_part2_provided_input():
+    expect(day01.increasesSlidingWindow(depths), 1797)
