3f341eba7d52c2e0b7b458b6047e5b960dc0b7e9
Jacob Hamblin <jacob@hamblin.cc>
2021-12-04 04:01:12 -0700

    cleanup

--- puzzles/day04.py
+++ puzzles/day04.py
@@ -1,87 +1,46 @@
 def has_bingo(board, cur_nums):
-    for line in board["values"]:
-        selected_values = [v.get("value") for v in line if v.get("selected")]
+    for line in board:
+        selected_values = [v for v in line if v in cur_nums]
         if len(selected_values) == 5:
             return True
-    for j in range(len(board["values"][0])):
-        col = [board["values"][i][j] for i in range(len(board["values"]))]
-        selected_values = [v.get("value") for v in col if v.get("selected")]
+    for j in range(len(board[0])):
+        col = [board[i][j] for i in range(len(board))]
+        selected_values = [v for v in col if v in cur_nums]
         if len(selected_values) == 5:
             return True
     return False
 
 
-def get_return_value(board, final_num):
-    unmarked_sum = sum(
-        [
-            v.get("value")
-            for line in board["values"]
-            for v in line
-            if not v.get("selected")
-        ]
-    )
+def get_return_value(board, cur_nums, final_num):
+    unmarked_sum = sum([v for line in board for v in line if not v in cur_nums])
     return unmarked_sum * final_num
 
 
-def board_prep(boards):
-    boards_dict = {}
-    for i in range(len(boards)):
-        board = boards[i]
-        data = {"presence": set(), "values": [], "locations": {}}
-
-        # j is a row index
-        for j in range(len(board)):
-            line = board[j]
-            values_line = []
-
-            # k is a col index
-            for k in range(len(line)):
-                value = line[k]
-                data["presence"].add(value)
-                values_line.append({"value": value, "selected": False})
-                data["locations"][value] = (j, k)
-            data["values"].append(values_line)
-        boards_dict[i] = data
-    return boards_dict
-
-
 def first_bingo(selected_nums, boards):
-    boards_dict = board_prep(boards)
-
     cur_nums = set()
     for num in selected_nums:
         cur_nums.add(num)
 
-        # i is board index
-        for i in range(len(boards_dict.keys())):
-            board = boards_dict[i]
-            if num in board["presence"]:
-                row, col = board["locations"][num]
-                board["values"][row][col]["selected"] = True
+        for board in boards:
             if len(cur_nums) > 4:
                 res = has_bingo(board, cur_nums)
                 if res:
-                    return get_return_value(board, num)
+                    return get_return_value(board, cur_nums, num)
 
 
 def last_bingo(selected_nums, boards):
-    boards_dict = board_prep(boards)
-
     bingo_boards = set()
     cur_nums = set()
     for num in selected_nums:
         cur_nums.add(num)
 
         # i is board index
-        for i in range(len(boards_dict.keys())):
-            board = boards_dict[i]
+        for i in range(len(boards)):
+            board = boards[i]
             if not i in bingo_boards:
-                if num in board["presence"]:
-                    row, col = board["locations"][num]
-                    board["values"][row][col]["selected"] = True
                 if len(cur_nums) > 4:
                     res = has_bingo(board, cur_nums)
                     if res:
                         bingo_boards.add(i)
-                        if len(bingo_boards) == len(boards_dict.keys()):
-                            return get_return_value(board, num)
+                        if len(bingo_boards) == len(boards):
+                            return get_return_value(board, cur_nums, num)
